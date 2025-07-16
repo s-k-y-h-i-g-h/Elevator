8ffc785244747d7cb91ccc4bde9655e74a8d2799
George Zankevich <zankevich00@outlook.com>
2025-07-16 19:45:03 +0100

    Initial solution and projects

---
+++ Elevator.Maui/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Elevator.Maui;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Elevator.Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Elevator.Maui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
