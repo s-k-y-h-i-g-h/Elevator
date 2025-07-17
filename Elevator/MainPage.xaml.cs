173f19782119cba96be3f5a8aa83eb6c4d94d34f
George Zankevich <zankevich00@outlook.com>
2025-07-17 23:02:51 +0100

    Project template

---
+++ Elevator/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Elevator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Elevator/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Elevator
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
