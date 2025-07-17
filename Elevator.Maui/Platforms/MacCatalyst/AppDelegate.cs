62d1b489e30a7af4b12bf2ff51535e9ed55228d8
George Zankevich <zankevich00@outlook.com>
2025-07-17 23:02:10 +0100

    AGAIN

--- Elevator.Maui/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace Elevator.Maui;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
--- Elevator.Shared/Services/DTOs/AuthResult.cs
+++
@@ -1,9 +0,0 @@
-namespace Elevator.Shared.Services.DTOs;
-
-public class AuthResult
-{
-    public bool Success { get; set; }
-    public string? Token { get; set; } // JWT token for MAUI
-    public UserDto? User { get; set; }
-    public string? ErrorMessage { get; set; }
-}
