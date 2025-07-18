200d8de32633470ed3cfc47bbcdeccd113724135
George Zankevich <zankevich00@outlook.com>
2025-07-18 13:41:08 +0100

    Template

---
+++ MauiHybridAuth/MauiHybridAuth.Web/Services/FormFactor.cs
@@ -0,0 +1,17 @@
+using MauiHybridAuth.Shared.Services;
+
+namespace MauiHybridAuth.Web.Services
+{
+    public class FormFactor : IFormFactor
+    {
+        public string GetFormFactor()
+        {
+            return "Web";
+        }
+
+        public string GetPlatform()
+        {
+            return Environment.OSVersion.ToString();
+        }
+    }
+}
---
+++ MauiHybridAuth/MauiHybridAuth/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiHybridAuth
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
